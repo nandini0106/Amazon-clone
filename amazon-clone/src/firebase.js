130c39b5c0bb011497d681c3c05bfeca22fe99b6
nandini0106 <67344563+nandini0106@users.noreply.github.com>
2022-01-28 22:39:02 +0530

    first commit

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCeP1kl4P0948yUV6SAf_-xPGZfVN_EhCk",
+    authDomain: "challenge-fdf48.firebaseapp.com",
+    projectId: "challenge-fdf48",
+    storageBucket: "challenge-fdf48.appspot.com",
+    messagingSenderId: "145975290024",
+    appId: "1:145975290024:web:ffb6c1dc2f44a511c73a9d",
+    measurementId: "G-HY6V7ZH5KN"
+  };
